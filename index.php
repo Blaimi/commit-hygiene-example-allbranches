601b2e10ea46f09523e9da38840b0a4baadeae1c
Matthias Blümel <blaimi@blaimi.de>
2023-09-02 23:07:21 +0200

    codestyle
    Change-Id: I027d3351fc77193d610b24d200c75cc733639717

--- index.php
+++ index.php
@@ -5,19 +5,21 @@ if (empty($_GET['firstName'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>commit hygiene example</title>
-	</head>
+    <head>
+        <title>commit hygiene example</title>
+    </head>
     <body>
         <form>
             <label for="firstName">first name</label>
-            <input id="firstName" name="firstName" /><br>
-            <label for="lastName">last name</label><input id="lastName" name="lastName" /><br>
+            <input id="firstName" name="firstName"/><br>
+            <label for="lastName">last name</label>
+            <input id="lastName" name="lastName"/><br>
             <label for="street">street</label>
-            <input id="street" name="street" /><br>
-            <label for="zip">zip</label><input id="zip" name="zip" /><br>
+            <input id="street" name="street"/><br>
+            <label for="zip">zip</label>
+            <input id="zip" name="zip"/><br>
             <label for="city">city</label>
-            <input id="city" name="city" /><br>
+            <input id="city" name="city"/><br>
             <button type="submit">submit</button>
         </form>
     </body>
@@ -25,13 +27,15 @@ if (empty($_GET['firstName'])) {
 <?php
 } else {
     $user = new User($_GET);
-    ?>
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html lang="en">
-	<head>    <title>commit hygiene example</title> </head>   
+    <head><title>commit hygiene example</title></head>
     <body>
-        <h1><?=$user->getFirstName() . ' ' . $user->getLastName()?></h1>
-        <p><?=$user->getStreet()?><br><span style="font-size: larger"><?=$user->getZip()?></span> <?=$user->getCity()?>
+        <h1><?= $user->getFirstName() . ' ' . $user->getLastName() ?></h1>
+        <p>
+            <?= $user->getStreet() ?><br>
+            <span style="font-size: larger"><?= $user->getZip() ?></span> <?= $user->getCity() ?>
         </p>
         <form>
             <button type="submit" formaction="index.php">new</button>
@@ -41,5 +45,3 @@ if (empty($_GET['firstName'])) {
 <?php
 }
 ?>
-
-
