41f3b74591bfb5f2f096b492a4e96c6e50bd0676
Matthias Blümel <blaimi@blaimi.de>
2023-09-03 00:00:24 +0200

    abstract Address from User
    Change-Id: If105072e08c6a8e7686b4aa6227e2b0e630ed514

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ if (empty($_GET['firstName'])) {
         <form>
             <label>first name <input name="firstName"/></label><br>
             <label>last name <input name="lastName"/></label><br>
-            <label>street <input name="street"/></label><br>
-            <label>zip <input name="zip"/></label><br>
-            <label>city <input name="city"/></label><br>
+            <label>street <input name="address[street]"/></label><br>
+            <label>zip <input name="address[zip]"/></label><br>
+            <label>city <input name="address[city]"/></label><br>
             <button type="submit">submit</button>
         </form>
     </body>
@@ -29,8 +29,8 @@ if (empty($_GET['firstName'])) {
     <body>
         <h1><?= htmlspecialchars($user->getFirstName() . ' ' . $user->getLastName(), ENT_HTML5) ?></h1>
         <p>
-            <?= htmlspecialchars($user->getStreet(), ENT_HTML5) ?><br>
-            <span style="font-size: larger"><?= htmlspecialchars($user->getZip(), ENT_HTML5) ?></span> <?= htmlspecialchars($user->getCity(), ENT_HTML5) ?>
+            <?= htmlspecialchars($user->getAddress()->getStreet(), ENT_HTML5) ?><br>
+            <span style="font-size: larger"><?= htmlspecialchars($user->getAddress()->getZip(), ENT_HTML5) ?></span> <?= htmlspecialchars($user->getAddress()->getCity(), ENT_HTML5) ?>
         </p>
     </body>
 </html>
